39c117a4cd0251a980f3f2ae474798d40137c320
Abhishek Kumar <royabhi647@gmail.com>
2024-04-11 18:14:32 +0530

    hithub

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
 const jsonfile = require('jsonfile');
 const moment = require('moment');
 const simpleGit = require('simple-git');
+// const random = require('random');
 
 const FILE_PATH = './data.json';
 
-const DATE = moment().subtract(1, 'd').format();
+const makeCommit = (n) => {
+    if (n === 0) {
+        return simpleGit().push(['origin', 'main'], (err, result) => {
+            if (err) {
+              console.error('Error pushing to remote repository:', err);
+            } else {
+              console.log('Pushed changes to remote repository:', result);
+            }
+          });
+          
+    }
 
-const data = {
-    date: DATE
+    import('random').then((random) => {
+        // Use the random module here
+        const x = Math.floor(Math.random() * 55)
+        const y = Math.floor(Math.random() * 7);
+        const DATE = moment().subtract(1, 'y').add(1, 'd').add(x, 'w').add(y, 'd').format();
+
+        const data = {
+            date: DATE
+        }
+        console.log(DATE);
+
+        jsonfile.writeFile(FILE_PATH, data, () => {
+            simpleGit().add([FILE_PATH]).commit(DATE, { '--date': DATE },
+                makeCommit.bind(this, --n));
+        });
+    }).catch((err) => {
+        // Handle error
+        console.log(err);
+    });
+
 }
 
-jsonfile.writeFile(FILE_PATH, data);
-
-simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE}).push();
+makeCommit(500);
